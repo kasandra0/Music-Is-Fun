fc4ed99e3b7d5e73b36622e8c77aae2fd456f4c0
Kasandra Wolf <wolf.kasandra@gmail.com>
2018-11-11 23:16:09 -0700

    done

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -9,16 +9,15 @@ function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
   let template = ''
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < results.length; i++) {
     let song = results[i] // style="height: 393px"
     template += `<div class="col-12 col-md-3 offset-md-1 card-img-top text-center" id="song-card"> 
         <img class="card-img-top" src="${song.albumArt}" style="max-height:245px; max-width:245px"/>
         <div class="card-body">
           <h4 class="card-title" id="overflow-text">${song.title}</h4>
-          <h5 id="overflow-text">${song.artist}  -<span> ${song.collection}</span></h5>
-          <h7>$${song.price}</h7>
-          <audio style="width: 100%;" controls src="${song.preview}"></audio>
-  
+          <h5 id="overflow-text">${song.artist}  - ${song.collection}</h5> <br/>
+          <h7>$${song.price}</h7> <br/>
+          <audio style="width: 90%;" controls src="${song.preview}"></audio>
         </div>
                   </div >
                   `
